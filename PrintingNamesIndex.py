16c111faed2625d8af2a963028e1ed7445806b19
Ralph <02travis.ralph@gulllakecs.org>
2024-03-14 14:02:46 -0400

    printing names index - called and stable
    Pre ChatGPT

--- PrintingNamesIndex.py
+++ PrintingNamesIndex.py
@@ -3,3 +3,10 @@
 # Program: Password Generator
 # Resources: https://www.zdnet.com/article/how-to-write-better-chatgpt-prompts-in-5-steps/
 
+names = ["Corey" , "Chris" , "Travis" , "Dave"]
+
+index = 0
+for name in names:
+    print(index,name)
+    index += 1
+    
